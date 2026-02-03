267f75f5b44d4e0e0b27348dfee54a42a8c065fc
Justus Kraft <hallo@justuskraft.de>
2026-02-03 12:33:55 +0100

    feat: add empty state to snippet

--- snippets/moments.php
+++ snippets/moments.php
@@ -1,11 +1,18 @@
-<ul class="moments-grid">
-	<?php foreach (collection('moments/all') as $moment) : ?>
-		<li>
-			<a href="<?= $moment->url() ?>" class="moment">
-				<?php
-                snippet('moments-image', ['moment' => $moment, 'type' => 'grid']);
-	    ?>
-			</a>
-		</li>
-	<?php endforeach; ?>
-</ul>
+<?php
+$moments = collection('moments/all');
+?>
+<?php if ($moments->count() === 0) : ?>
+    <p class="moment-grid-empty-text"><?= t('femundfilou.kirby-moments.no-moments') ?></p>
+<?php else: ?>
+    <ul class="moments-grid">
+        <?php foreach ($moments as $moment) : ?>
+            <li>
+                <a href="<?= $moment->url() ?>" class="moment">
+                    <?php
+                    snippet('moments-image', ['moment' => $moment, 'type' => 'grid']);
+                    ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
