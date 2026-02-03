de519b6da0cb28bb37ae22a6474f260b2d8eddce
Justus Kraft <hallo@justuskraft.de>
2026-02-03 13:10:23 +0100

    feat: use text field in grid and lightbox

--- snippets/moments-image-footer.php
+++ snippets/moments-image-footer.php
@@ -1,4 +1,11 @@
 <?php if ($page->date()->isNotEmpty()) : ?>
-	<?php snippet('moments-icon/clock'); ?>
-	<time class="moment-time" datetime="<?= $page->date()->toMomentsTimestamp() ?>"><?= $page->date()->toMomentsDate() ?></time>
+    <?php if ($page->text()->isNotEmpty()) : ?>
+        <div class="moments-image-footer__text">
+            <p><?= $page->text() ?></p>
+        </div>
+    <?php endif; ?>
+    <div class="moments-image-footer__time">
+        <?php snippet('moments-icon/clock'); ?>
+        <time class="moment-time" datetime="<?= $page->date()->toMomentsTimestamp() ?>"><?= $page->date()->toMomentsDate() ?></time>
+    </div>
 <?php endif; ?>
